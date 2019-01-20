19e32a5aa012b089fb806df75b819a243eda99cc
tomouchi <29200586+tomouchi@users.noreply.github.com>
2019-01-20 21:27:22 +0900

    Lyricsを表示する際のアコーディオンを実装

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -25,7 +25,7 @@ export default {
   mounted() {
     this.$root.$data.pageName = 'TOP';
 
-    $('.flexslider').flexslider({
+    jQuery('.flexslider').flexslider({
       directionNav: false,
       animationSpeed: 3000,
     });
